a11488825964a102bc548396dbc7b0bc9cbbc2ce
Wenhao Nie <slanlxd@gmail.com>
2022-02-05 23:35:56 +0800

    Adding the peer review material

---
+++ polysum.py
@@ -0,0 +1,10 @@
+import math
+def polysum(n,s):
+    """
+    Calculate the polysum of a regular polygon
+    n - number of sides
+    s - length of each side
+    """
+    area = 0.25*n*s**2/math.tan(math.pi/n)
+    perimeter = n*s
+    return round(area+perimeter**2,4)
